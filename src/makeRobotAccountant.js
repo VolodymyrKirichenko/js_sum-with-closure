51086ca454673bedf0ac3180df6c697022a17718
VolodymyrKirichenko <109915550+VolodymyrKirichenko@users.noreply.github.com>
2022-10-11 14:03:09 +0300

    solution (#1)
    * solution
    
    * changed variables names

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  const sum = (num1) => {
+    count++;
+
+    const x = (num2) => {
+      if (count < 3 || count % 2 !== 0) {
+        return num1 + num2;
+      }
+
+      return 'Bzzz... Error!';
+    };
+
+    return x;
+  };
+
+  return sum;
 }
 
 module.exports = makeRobotAccountant;
